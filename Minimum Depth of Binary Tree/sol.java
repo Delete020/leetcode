5017798509aabc467bc0691cd2fa85b6ed2af515
Delete020 <delete020@protonmail.com>
2020-10-23 19:59:18 +0800

    #111

---
+++ Minimum Depth of Binary Tree/sol.java
@@ -0,0 +1,33 @@
+public class sol {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null) {
+            return 1 + minDepth(root.right);
+        }
+        if (root.right == null) {
+            return 1 + minDepth(root.left);
+        }
+        return Math.min(1 + minDepth(root.left), 1 + minDepth(root.right));
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
