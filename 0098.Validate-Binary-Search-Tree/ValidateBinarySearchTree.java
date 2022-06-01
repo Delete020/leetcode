bcf63432739075fe89ea2c5dd5266b8e424f35fd
Delete020 <delete020@protonmail.com>
2022-06-01 12:52:32 +0800

    #0098

---
+++ 0098.Validate-Binary-Search-Tree/ValidateBinarySearchTree.java
@@ -0,0 +1,40 @@
+/**
+ * @author Delete020
+ * @since 6/1/22 12:51 PM
+ */
+public class ValidateBinarySearchTree {
+    long pre = Long.MIN_VALUE;
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (!isValidBST(root.left)) {
+            return false;
+        }
+        if (root.val <= pre) {
+            return false;
+        }
+        pre = root.val;
+        return isValidBST(root.right);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
