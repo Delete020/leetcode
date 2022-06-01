89f09a8cb67cade75249f65b0a4453d0e0c5bfd4
Delete020 <delete020@protonmail.com>
2022-06-01 12:19:08 +0800

    #0104

---
+++ 0104.Maximum-Depth-of-Binary-Tree/MaximumDepthofBinaryTree.java
@@ -0,0 +1,31 @@
+/**
+ * @author Delete020
+ * @since 6/1/22 12:17 PM
+ */
+public class MaximumDepthofBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
