4445064aa5e86ca9e3e071a3a729336b86ea16f3
Delete020 <delete020@protonmail.com>
2022-06-02 18:06:39 +0800

    #0701

---
+++ 0701.Insert-into-a-Binary-Search-Tree/InsertintoaBinarySearchTree.java
@@ -0,0 +1,36 @@
+/**
+ * @author Delete020
+ * @since 6/2/22 6:05 PM
+ */
+public class InsertintoaBinarySearchTree {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
