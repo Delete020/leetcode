0f18fe09e566288f472aedae31da7976f1364dad
Delete020 <delete020@protonmail.com>
2022-06-06 01:52:32 +0800

    #0236

---
+++ 0236.Lowest-Common-Ancestor-of-a-Binary-Tree/LowestCommonAncestorofaBinaryTree.java
@@ -0,0 +1,35 @@
+/**
+ * @author Delete020
+ * @since 6/6/22 1:51 AM
+ */
+public class LowestCommonAncestorofaBinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == p.val || root.val == q.val) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left == null && right == null) {
+            return null;
+        }
+        return left == null ? right : left;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
