116b6c595224ac55d20eb1db8876e7bd8e5852a2
Delete020 <delete020@protonmail.com>
2022-06-21 21:51:00 +0800

    #0669

---
+++ 0669.Trim-a-Binary-Search-Tree/TrimaBinarySearchTree.java
@@ -0,0 +1,42 @@
+/**
+ * @author Delete020
+ * @since 6/21/22 9:47 PM
+ */
+public class TrimaBinarySearchTree {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        }
+        if (root.val > high) {
+            return trimBST(root.left, low, high);
+        }
+
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
