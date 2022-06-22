2881e36efba838199f16e91766b9229dbc4e6ade
Delete020 <delete020@protonmail.com>
2022-06-22 21:42:50 +0800

    #0509

---
+++ 0509.Fibonacci-Number/FibonacciNumber.java
@@ -0,0 +1,18 @@
+/**
+ * @author Delete020
+ * @since 6/22/22 9:42 PM
+ */
+public class FibonacciNumber {
+    public int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
