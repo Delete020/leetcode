22015c9068bd2601387a8db8442726e175b8c459
Delete020 <delete020@protonmail.com>
2022-07-06 22:27:40 +0800

    #0005

---
+++ 0005.Longest-Palindromic-Substring/LongestPalindromicSubstring.java
@@ -0,0 +1,24 @@
+/**
+ * @author Delete020
+ * @since 07/06/22 22:26 PM
+ */
+public class LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+        String res = "";
+        for (int i = 0; i < s.length(); i++) {
+            String s1 = palindrome(s, i, i);
+            String s2 = palindrome(s, i, i + 1);
+            res = res.length() > s1.length() ? res : s1;
+            res = res.length() > s2.length() ? res : s2;
+        }
+        return res;
+    }
+
+    private String palindrome(String s, int l, int r) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            r++;
+            l--;
+        }
+        return s.substring(l + 1, r);
+    }
+}
