b353a4e62faf53b6bbaffa64de571aa6fee35c28
Delete020 <delete020@protonmail.com>
2022-07-13 21:12:38 +0800

    #0048

---
+++ 0048.Rotate-Image/RotateImage.java
@@ -0,0 +1,24 @@
+/**
+ * @author Delete020
+ * @since 07/13/22 21:11 PM
+ */
+public class RotateImage {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+}
