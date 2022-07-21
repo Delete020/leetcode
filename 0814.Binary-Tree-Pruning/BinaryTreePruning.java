28bff708f1ac95e12a01df8d87192c2703f41aae
Delete020 <delete020@protonmail.com>
2022-07-21 21:47:19 +0800

    #0814

---
+++ 0814.Binary-Tree-Pruning/BinaryTreePruning.java
@@ -0,0 +1,34 @@
+/**
+ * @author Delete020
+ * @since 07/21/22 21:46 PM
+ */
+public class BinaryTreePruning {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) return null;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if (root.right == null && root.left == null && root.val == 0) {
+            return null;
+        }
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
