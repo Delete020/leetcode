0ec571f26b79b898a058b468bb6e5e5354e9fa23
Delete020 <delete020@protonmail.com>
2022-08-08 21:45:16 +0800

    #0117

---
+++ 0117.Populating-Next-Right-Pointers-in-Each-Node-II/PopulatingNextRightPointersinEachNodeII.java
@@ -0,0 +1,53 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author Delete020
+ * @since 08/08/22 21:43 PM
+ */
+public class PopulatingNextRightPointersinEachNodeII {
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; ) {
+                Node curr = queue.remove();
+                curr.next = ++i == size ? null : queue.peek();
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                }
+            }
+        }
+
+        return root;
+    }
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+}
