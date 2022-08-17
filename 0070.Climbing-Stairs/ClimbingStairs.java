558298a872b2bd74e190fdab5c90e01996f09371
Delete020 <delete020@protonmail.com>
2022-08-17 21:41:37 +0800

    #0070

---
+++ 0070.Climbing-Stairs/ClimbingStairs.java
@@ -0,0 +1,18 @@
+/**
+ * @author Delete020
+ * @since 08/17/22 21:40 PM
+ */
+public class ClimbingStairs {
+    int cnt = 0;
+
+    public int climbStairs(int n) {
+        if (n <= 2) return n;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
