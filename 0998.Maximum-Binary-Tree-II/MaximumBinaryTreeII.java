cc60bdb3ce198d72e9623fc65dbe0a7388aefe3b
Delete020 <delete020@protonmail.com>
2022-08-30 22:12:52 +0800

    #0998

---
+++ 0998.Maximum-Binary-Tree-II/MaximumBinaryTreeII.java
@@ -0,0 +1,32 @@
+/**
+ * @author Delete020
+ * @since 08/30/22 22:11 PM
+ */
+public class MaximumBinaryTreeII {
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        if (root == null || root.val < val) {
+            return new TreeNode(val, root, null);
+        }
+        root.right = insertIntoMaxTree(root.right, val);
+        return root;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
