5f03d37be70c9c0d6a675f7dbdaf165b346b3f84
Delete020 <delete020@protonmail.com>
2022-10-02 22:08:56 +0800

    #0777

---
+++ 0777.Swap-Adjacent-in-LR-String/SwapAdjacentinLRString.java
@@ -0,0 +1,20 @@
+/**
+ * @author Delete020
+ * @since 10/02/22 22:08 PM
+ */
+public class SwapAdjacentinLRString {
+    public boolean canTransform(String start, String end) {
+        int n = start.length(), i = 0, j = 0;
+        while (i < n || j < n) {
+            while (i < n && start.charAt(i) == 'X') i++;
+            while (j < n && end.charAt(j) == 'X') j++;
+            if (i == n || j == n) return i == j;
+            if (start.charAt(i) != end.charAt(j)) return false;
+            if (start.charAt(i) == 'L' && i < j) return false;
+            if (start.charAt(i) == 'R' && i > j) return false;
+            i++;
+            j++;
+        }
+        return i == j;
+    }
+}
