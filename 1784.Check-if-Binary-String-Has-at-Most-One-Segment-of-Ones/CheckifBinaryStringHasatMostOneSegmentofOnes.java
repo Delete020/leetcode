9ae3aa95131ee4d2ba80400b0740c25418a0970d
Delete020 <delete020@protonmail.com>
2022-10-03 17:17:13 +0800

    #1784

---
+++ 1784.Check-if-Binary-String-Has-at-Most-One-Segment-of-Ones/CheckifBinaryStringHasatMostOneSegmentofOnes.java
@@ -0,0 +1,9 @@
+/**
+ * @author Delete020
+ * @since 10/03/22 17:16 PM
+ */
+public class CheckifBinaryStringHasatMostOneSegmentofOnes {
+    public boolean checkOnesSegment(String s) {
+        return !s.contains("01");
+    }
+}
