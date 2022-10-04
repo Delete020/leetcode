61abe675fec9142c2f94c152238b928ee086f96f
Delete020 <delete020@protonmail.com>
2022-10-04 17:49:36 +0800

    #0020

---
+++ 0020.Valid-Parentheses/ValidParentheses.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+/**
+ * @author Delete020
+ * @since 10/04/22 17:47 PM
+ */
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (stack.isEmpty()) {
+                return false;
+            } else if (c == ')' && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == ']' && stack.peek() == '[') {
+                stack.pop();
+            } else if (c == '}' && stack.peek() == '{') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
